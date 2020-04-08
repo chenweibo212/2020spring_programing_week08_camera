d89de66b7d786ebb72790314cc42f34c8c268039
chenweibo212 <chenweibo212@gmail.com>
2020-04-08 13:53:59 -0400

    second commit

---
+++ app/app.js
@@ -0,0 +1,151 @@
+const p5 = require("p5");
+const Tone = require("tone");
+const StartAudioContext = require("startaudiocontext");
+
+// const synth = new Tone.Synth().toMaster();
+
+var noise = new Tone.Noise("brown").start();
+var autoFilter = new Tone.AutoFilter({
+  "frequency" : "4m",
+  "depth": 0.1,
+  "min" : 800,
+  "max" : 5000
+}).connect(Tone.Master);
+
+noise.connect(autoFilter);
+autoFilter.start();
+
+StartAudioContext(Tone.context);
+
+let faceapi;
+let detections;
+let width = window.outerWidth;
+let height = window.outerHeight-30;
+
+const detection_options = {
+    withLandmarks: true,
+    withDescriptors: false,
+    Mobilenetv1Model: 'models',
+    FaceLandmarkModel: 'models',
+    FaceRecognitionModel: 'models',
+    FaceExpressionModel: 'models',
+};
+
+const p5draw = (p) => {
+
+    let p5video;
+
+    let createImage, createDistortion;
+
+    let welcome = "subject has agreed to use webcam";
+    let text = "A VERY INTERACTIVE WEBPAGE";
+
+    function drawBox(detections) {
+        detections.forEach((detection) => {
+            const alignedRect = detection.alignedRect;
+
+            p.noFill();
+            p.stroke(0);
+            p.strokeWeight(2);
+            p.rect(
+                alignedRect._box._x,
+                alignedRect._box._y,
+                alignedRect._box._width,
+                alignedRect._box._height,
+            );
+        });
+    }
+
+    p.setup = () => {
+        p.createCanvas(width, height);
+        p.background(255);
+
+        p5video = p.createCapture(p.VIDEO);
+        p5video.size(width, height);
+        p5video.hide();
+
+        faceapi = ml5.faceApi(p5video, detection_options, modelReady);
+
+    }
+
+    p.draw = () => {
+        let textZoom;
+
+        //p.image(p5video, 0, 0, p.width, p.height);
+
+        let screenCssPixelRatio = (window.outerWidth - 8) / window.innerWidth;
+        console.log(screenCssPixelRatio);
+
+        if (detections) {
+                p.strokeWeight(0);
+                p.fill(255);
+                p.rect(0,0,width,height);
+
+                if(detections.length > 0){
+                    const detection = detections[0];
+                    const headWidth = detection.alignedRect._box._width;
+
+                    textZoom = p.map(headWidth, 250, 700, 15, 130);
+                    textBlur = p.map(headWidth, 250, 700, 0, 0.1);
+
+                    createImage = p.createGraphics(width,height-100);
+                    createImage.background = (255);
+                    createImage.push();
+                    createImage.textSize(textZoom);
+                    createImage.strokeWeight(0);
+                    createImage.fill(0);
+                    createImage.textAlign(createImage.CENTER);
+                    createImage.text(text, createImage.width/2, createImage.height/2);
+                    createImage.pop();
+                    createImage.loadPixels();
+                    createImage.updatePixels();
+
+                    if(headWidth < 250) {
+                        p.image(createImage, 0, 50);
+                    } else {
+                        for (let i = 0; i < height; i += 1) {
+                            let distort = p.map(p.noise(i * textBlur), 0, 1, 0, 99);
+                            createDistortion = createImage.get(0, i, width, 1);
+                            p.image(createDistortion, distort, i);
+                        }
+                    }
+                }
+            }
+
+            p.textSize(50);
+            p.strokeWeight(0);
+            p.typeText(welcome,0,25,50,20);
+        }
+
+    p.typeText = (sentence, n, x, y, speed) => {
+        if (n < (sentence.length)) {
+            p.text(sentence.substring(0, n+1), x, y);
+            n++;
+            setTimeout(function() {
+              p.typeText(sentence, n, x, y, speed)
+            }, speed);
+          }
+    }
+}
+
+function setup() {
+    const myp5 = new p5(p5draw, "main");
+}
+
+function modelReady() {
+    console.log("model ready!!!!");
+    faceapi.detect(gotResults);
+}
+
+function gotResults(err, results) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    detections = results;
+    faceapi.detect(gotResults);
+}
+
+// Calls the setup function when the page is loaded
+window.addEventListener("DOMContentLoaded", setup);
